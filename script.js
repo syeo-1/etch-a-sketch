b3cefbd07df320b95aa89661eb83a04ba4a60ef5
Sean Yeo <seansqyeo@gmail.com>
2020-04-25 12:00:40 -0700

    create simple 16x16 grid

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-document.querySelector
+let container = document.querySelector(".container");
+let dimensions = 16
+
+for (let i = 0 ; i < dimensions ; i++) {
+    for (let j = 0 ; j < dimensions ; j++) {
+        let div = document.createElement("div");
+        div.classList.add("square");
+        div.textContent = (i+1)*(j+1);
+        container.appendChild(div);
+    }
+}
+
